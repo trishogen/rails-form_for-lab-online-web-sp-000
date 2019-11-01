3e2b11c52942c109f306b84b669649e901ee708e
Trish Ogen <patriciaogen@gmail.com>
2019-11-01 19:38:19 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,7 +10,7 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_class_params)
     @school_class.save
-    redirect_to school_classes_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   def update
