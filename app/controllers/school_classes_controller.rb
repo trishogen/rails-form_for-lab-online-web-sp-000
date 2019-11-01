83449fd8c5238cb2f14359777b3bd1509cb6d13b
Trish Ogen <patriciaogen@gmail.com>
2019-11-01 19:36:11 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,4 +1,8 @@
 class SchoolClassesController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+  end
+  
   def show
     @school_class = SchoolClass.find(params[:id])
   end
