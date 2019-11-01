3ba233549da5be4ba57ae26c596490fb8320ffc5
Trish Ogen <patriciaogen@gmail.com>
2019-11-01 19:38:27 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -16,7 +16,7 @@ class SchoolClassesController < ApplicationController
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params)
-    redirect_to school_classes_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   private
