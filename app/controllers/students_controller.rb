0ab7e001e7accd9058616cdaf1b78f76858db0b6
Trish Ogen <patriciaogen@gmail.com>
2019-11-01 20:01:39 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
 class StudentsController < ApplicationController
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
   def create
     @student = Student.new(student_params)
     @student.save
